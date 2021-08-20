6ddacc4b69b45f1ac7e95230b96ace1df88b839d
Stephan Thiele <abuesswalker@gmail.com>
2021-08-20 08:50:54 +0200

    unittest code anpassung

--- test_chatbot.py
+++ test_chatbot.py
@@ -4,20 +4,24 @@ from chatbot_ai import Chatbot
 
 class TestChatBot(unittest.TestCase):
     def test_intelligent_answers(self):
-        """Test der intelligenten Antworten"""
-        self.__reaktionen = {"hallo": "aber hallo",
-                    "geht": "Was verstehst Du darunter",
-                    "schmeckt": "Ich habe keinen Geschmackssinn"}
-        __data = ["hallo du", "geht es dir gut", "schmeckt die Suppe"]
-        __bot = Chatbot(self.__reaktionen)
-        for sentence in __data:
-            __bot.set_Message(sentence)
-            self.__response = __bot.get_response()
-            __words = sentence.split()
-            for word in __words:
-                if word in self.__reaktionen:
-                    self.__rightResponse = self.__reaktionen[word]
-        self.assertEqual(self.__response, self.__rightResponse)
+        with open("v3/intens.json") as f:
+            """Test der intelligenten Antworten"""
+            self.__reaktionen = {"greetings": "Hallo",
+                        "goodbye": "Sehe Dich später",
+                        "age": "wie alt ist Jörg",
+                        "name": "Wie ist Dein Name",
+                        "shop": "Heute gibt es Kekse!",
+                        "hours": "Wann ist die Öffnungszeit"}
+            __data = ["Guten Tag", "Tschö", "Wie alt bist Du"]
+            __bot = Chatbot(self.__reaktionen)
+            for sentence in __data:
+                __bot.set_Message(sentence)
+                self.__response = __bot.get_response()
+                __words = sentence.split()
+                for word in __words:
+                    if word in self.__reaktionen:
+                        self.__rightResponse = self.__reaktionen[word]
+            self.assertEqual(self.__response, self.__rightResponse)
 
 
 if __name__ == '__main__':
